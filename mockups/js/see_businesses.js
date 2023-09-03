555e20ff574f2828a281d6791538a50dc3cca2e3
akavaramV <akavaram@sheridancollege.ca>
2023-09-03 09:58:24 -0400

    feat: updating code

--- mockups/js/see_businesses.js
+++ mockups/js/see_businesses.js
@@ -1,23 +1,47 @@
+// var videoContainer = document.getElementById("video-container");
+// var twoContainers = document.getElementById("two-containers");
+// var questionairreContainer = document.getElementById("questionairre-container"); 
+// var meetingContainer = document.getElementById("meeting-container"); 
+
+
+// function toggleVideoContainer() {
+//     twoContainers.hidden = !twoContainers.hidden;
+//     questionairreContainer.hidden = !questionairreContainer.hidden;
+//     videoContainer.hidden = !videoContainer.hidden;
+// }
+
+// function toggleQuestionairreContainer() {
+//     twoContainers.hidden = !twoContainers.hidden;
+//     meetingContainer.hidden = !meetingContainer.hidden;
+//     videoContainer.hidden = !videoContainer.hidden;
+// }
+
+// function toggleMeetingContainer() {
+//     twoContainers.hidden = !twoContainers.hidden;
+//     questionairreContainer.hidden = !questionairreContainer.hidden;
+//     videoContainer.hidden = !videoContainer.hidden;
+// }
+
 var videoContainer = document.getElementById("video-container");
-var twoContainers = document.getElementById("two-containers");
 var questionairreContainer = document.getElementById("questionairre-container"); 
 var meetingContainer = document.getElementById("meeting-container"); 
+var twoContainers = document.getElementById("two-containers");
 
-
-function toggleVideoContainer() {
-    twoContainers.hidden = !twoContainers.hidden;
-    questionairreContainer.hidden = !questionairreContainer.hidden;
-    videoContainer.hidden = !videoContainer.hidden;
+function hideAllContainers() {
+    videoContainer.hidden = true;
+    questionairreContainer.hidden = true;
+    meetingContainer.hidden = true;
 }
 
-function toggleQuestionairreContainer() {
-    twoContainers.hidden = !twoContainers.hidden;
-    meetingContainer.hidden = !meetingContainer.hidden;
-    videoContainer.hidden = !videoContainer.hidden;
-}
+function toggleContainer(containerType) {
+    hideAllContainers();
 
-function toggleMeetingContainer() {
-    twoContainers.hidden = !twoContainers.hidden;
-    questionairreContainer.hidden = !questionairreContainer.hidden;
-    videoContainer.hidden = !videoContainer.hidden;
+    if (containerType === 'video') {
+        videoContainer.hidden = false;
+    } else if (containerType === 'question') {
+        twoContainers.hidden = false;
+        questionairreContainer.hidden = false;
+    } else if (containerType === 'meetup') {
+        meetingContainer.hidden = false;
+    }
 }
