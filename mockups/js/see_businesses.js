110a407ba2cfd656f7bef15f57571b8144c1ca00
akavaramV <akavaram@sheridancollege.ca>
2023-10-01 23:53:35 -0400

    chore: formatted html and css setions

--- mockups/js/see_businesses.js
+++ mockups/js/see_businesses.js
@@ -1,27 +1,3 @@
-// var videoContainer = document.getElementById("video-container");
-// var twoContainers = document.getElementById("two-containers");
-// var questionairreContainer = document.getElementById("questionairre-container"); 
-// var meetingContainer = document.getElementById("meeting-container"); 
-
-
-// function toggleVideoContainer() {
-//     twoContainers.hidden = !twoContainers.hidden;
-//     questionairreContainer.hidden = !questionairreContainer.hidden;
-//     videoContainer.hidden = !videoContainer.hidden;
-// }
-
-// function toggleQuestionairreContainer() {
-//     twoContainers.hidden = !twoContainers.hidden;
-//     meetingContainer.hidden = !meetingContainer.hidden;
-//     videoContainer.hidden = !videoContainer.hidden;
-// }
-
-// function toggleMeetingContainer() {
-//     twoContainers.hidden = !twoContainers.hidden;
-//     questionairreContainer.hidden = !questionairreContainer.hidden;
-//     videoContainer.hidden = !videoContainer.hidden;
-// }
-
 var videoContainer = document.getElementById("video-container");
 var questionairreContainer = document.getElementById("questionairre-container"); 
 var meetingContainer = document.getElementById("meeting-container"); 
